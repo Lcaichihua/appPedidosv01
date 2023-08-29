248e69f4b51497fdd55b09c38505b9c2265a2eb7
Larry Caichihua Vergara <lcaichihua@gmail.com>
2023-08-29 01:03:14 -0500

    add style component register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductoListadoComponent } from './components/productos/producto-listado/producto-listado.component';
 import { ClienteListadoComponent } from './components/clientes/cliente-listado/cliente-listado.component';
+import { ProductoRegistroComponent } from './components/productos/producto-registro/producto-registro.component';
 
 const routes: Routes = [
   {path:'',component:ProductoListadoComponent},
   {path:'producto-listado',component:ProductoListadoComponent},
+  {path:'producto-registro',component:ProductoRegistroComponent},
   {path:'cliente-listado',component:ClienteListadoComponent}
 ];
 
